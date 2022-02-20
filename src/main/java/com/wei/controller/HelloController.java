3edd194bfc2295fd26c4d4e3f12ce23e395d179f
魏棋 <9445652+weiqi182183@user.noreply.gitee.com>
2022-02-20 06:08:52 +0000

    update src/main/java/com/wei/controller/HelloController.java.

--- src/main/java/com/wei/controller/HelloController.java
+++ src/main/java/com/wei/controller/HelloController.java
@@ -18,6 +18,7 @@ public class HelloController {
         System.out.println("master test");
         System.out.println("hot-fix");
          System.out.println("pull-test");
+         System.out.println("gitee pull-test");
         return "hello spring boot。"+name;
     }
 
